ec74339a50d290714e24a25c94765739a50cc62e
Ashish Patil <ashishpatil2366@gmail.com>
2025-12-28 21:31:19 +0530

    updated server.js

--- project_backend/server.js
+++ project_backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 // Routers
 const userRouter = require('./routes/user');
+const courseRouter = require('./routes/course');
+
 
 // Middleware
 const app = express();
@@ -13,6 +15,8 @@ app.use(authUser);
 
 // Mount Routes
 app.use('/user', userRouter);
+app.use('/course', courseRouter);
+
 
 app.listen(4000, 'localhost', () => {
     console.log('Server is running on port 4000');
